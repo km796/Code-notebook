4d73af0ffa52d8384468da894f5c36e04c04b9cb
km796 <31836577+km796@users.noreply.github.com>
2022-04-24 14:52:49 +0900

    Fetch user action added

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -11,3 +11,11 @@ export const fetchPosts = () => {
     }
 }
 
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`./users/${id}`)
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    })
+
+}
