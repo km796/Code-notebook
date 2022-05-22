eed397dd77d2b83bf41588b3eff0065ad17cf337
km796 <31836577+km796@users.noreply.github.com>
2022-05-22 11:15:24 +0900

    Mappable interface created for implicit type checks

--- typescriptPractice/maps/src/CustomMap.ts
+++ typescriptPractice/maps/src/CustomMap.ts
@@ -1,6 +1,13 @@
 import { Company } from "./Company";
 import { User } from "./User";
 
+interface Mappable {
+    location: {
+        lat: number,
+        lng: number
+    }
+}
+
 export class CustomMap {
     private googleMap: google.maps.Map
 
@@ -14,22 +21,12 @@ export class CustomMap {
         });
     }
 
-    addUserMarker(user: User) {
+    addMarker(mappable: Mappable) {
         new google.maps.Marker({
             map: this.googleMap,
             position: {
-                lat: user.location.lat,
-                lng: user.location.lng
-            }
-        })
-    }
-    
-    addCompanyMarker(company: Company) {
-        new google.maps.Marker({
-            map: this.googleMap,
-            position: {
-                lat: company.location.lat,
-                lng: company.location.lng
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
             }
         })
     }
