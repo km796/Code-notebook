f9d4da23fa32e848bc6d1805e6f54c8cfd073a4a
km796 <31836577+km796@users.noreply.github.com>
2022-05-22 11:48:41 +0900

    Showing popup window on marker click

--- typescriptPractice/maps/src/CustomMap.ts
+++ typescriptPractice/maps/src/CustomMap.ts
@@ -22,12 +22,19 @@ export class CustomMap {
     }
 
     addMarker(mappable: Mappable) {
-        new google.maps.Marker({
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
                 lng: mappable.location.lng
             }
+        })
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: 'Hi There!'
+            })
+
+            infoWindow.open(this.googleMap, marker)
         })
     }
 }
