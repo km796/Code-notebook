cb04902842632251c576a4a55e6de22cdbf71770
km796 <31836577+km796@users.noreply.github.com>
2022-05-23 16:03:56 +0900

    Typescript around props and states practice

---
+++ rts/src/index.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom'
+import GuestList from './state/GuestList'
+import UserSearch from './state/UserSearch'
+
+
+const App = () => {
+    return <div>
+        <UserSearch />
+    </div>
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
