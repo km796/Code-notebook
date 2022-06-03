1561a4aabc241695bb4123a4b118123c4a8e970f
km796 <31836577+km796@users.noreply.github.com>
2022-06-03 17:22:56 +0900

    Extracting a bundling logic

--- jbook/jbook/src/index.tsx
+++ jbook/jbook/src/index.tsx
@@ -1,67 +1,18 @@
-import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom";
 import { useState, useEffect, useRef } from "react";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugins";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+
 import CodeEditor from "./components/code-editor";
+import Preview from "./components/preview";
+import bundle from "./bundler";
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "./esbuild.wasm",
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    const output = await bundle(input);
+    setCode(output);
   };
-
-  const html = `
-    <html>
-        <head></head>
-        <body>
-            <div id="root"></div>
-            <script>
-                window.addEventListener('message', (event) => {
-                    try {
-                        eval(event.data);
-                    } catch (err) {
-                        const root = document.querySelector('#root');
-                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
-                    }
-                    
-                }, false);
-            </script>
-        </body>
-    </html>
-    `;
 
   return (
     <div>
@@ -69,14 +20,10 @@ const App = () => {
         initialValue="const a = 1"
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+      <Preview code={code} />
     </div>
   );
 };
